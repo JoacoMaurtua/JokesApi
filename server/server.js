296b8ec04e38b47d375800a6694ebd1a6ba7cd29
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-01 19:59:02 -0500

    server loading

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express() //accediendo a las funcionalidades de express
+
+const PORT = 8000;
+
+//midlewares para realizar consulta POST
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+
+
+
+
+
+
+
+
+//comprobar que el servidor esta corriendo
+app.listen(PORT,()=>{
+  console.log(`1: Server loading on port: ${PORT}`);
+})
